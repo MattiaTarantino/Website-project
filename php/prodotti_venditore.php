e7dfb97ee383f4a1f10babb07a6bd4b62150ee20
JacopoDellepiane <jacopodellepiane@icloud.com>
2023-05-01 17:58:26 +0200

    slide symobl style

--- php/prodotti_venditore.php
+++ php/prodotti_venditore.php
@@ -12,7 +12,6 @@ $venditore = $_SESSION['venditore'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.css" />
     <link rel="stylesheet" href="../css/search.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <title>Shopwise - I tuoi prodotti</title>
     <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
